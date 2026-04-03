d3bc9e64be90fa53292a13a90c783cab7be829fc
Tine Hbr <tine.hab@gmail.com>
2026-04-03 12:16:35 +0200

    typo

--- mysite_new/blog/models.py
+++ mysite_new/blog/models.py
@@ -16,6 +16,7 @@ class Post(models.Model):
     def __str__(self):
        return self.title
 
+#huiswerk Les 4
 class Voetbalspelers(models.Model):
     # De naam van de voetballer en club zijn gewoon tekstvelden
     naam_voetballer = models.CharField(max_length=200) 
@@ -27,7 +28,7 @@ class Voetbalspelers(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)    #mag leeg veld
 
-    #"verzendknop": vult pub_date in & schrijft talles nr db"
+    #"verzendknop": vult pub_date in & schrijft alles nr db"
     def publish(self):  #actie op dit specifieke object
         self.published_date = timezone.now()    #vervang None/NULL in published_date met huidig tijdstip van deze specifiek speler/object
         self.save() #ingebouwde fctie v models.Model; werk alle gegevens in db bij 
